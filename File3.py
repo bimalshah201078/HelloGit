97d8b3126d5072b1b68be4cfda835b3479fd7b28
bimalshah201078 <bimal.shah@howdens.com>
2024-06-03 13:18:43 +0100

    first hello world

---
+++ File3.py
@@ -0,0 +1,7 @@
+greeting = "Hello, World!"
+number = 42
+pi = 3.14159
+
+print(greeting)
+print("The answer to life, the universe, and everything is:", number)
+print("The value of pi is approximately:", pi)
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HelloGit
+This is the repository of my first exercise for creating Git hub.
+Java file for HelloWorld"
